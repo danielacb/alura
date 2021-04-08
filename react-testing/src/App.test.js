982b8de7c3866478b3d69a2b7137cd78070d74e5
Daniela C. Barbosa <contato@danielacb.com>
2021-04-07 22:53:33 -0300

    test calcularNovoSaldo function

--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import App from "./app";
+import App, { calcularNovoSaldo } from "./app";
 
 describe("App component", () => {
   describe("When I open the app", () => {
@@ -21,6 +21,34 @@ describe("App component", () => {
       render(<App />);
 
       expect(screen.getByText("Realizar operação")).toBeInTheDocument();
+    });
+  });
+
+  describe("When I make a transation", () => {
+    describe("When make a deposit", () => {
+      it("should add money to the account balance", () => {
+        const transaction = {
+          transacao: "saque",
+          valor: 50,
+        };
+
+        const newBalance = calcularNovoSaldo(transaction, 150);
+
+        expect(newBalance).toBe(100);
+      });
+    });
+
+    describe("When I withdraw money", () => {
+      it("should subtract money from the account balance", () => {
+        const transaction = {
+          transacao: "deposito",
+          valor: 50,
+        };
+
+        const newBalance = calcularNovoSaldo(transaction, 150);
+
+        expect(newBalance).toBe(200);
+      });
     });
   });
 });
