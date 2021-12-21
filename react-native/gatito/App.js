e77285ce2b082b99070e64a0a59102445b8c3dbb
Daniela Barbosa <contato@danielacb.com>
2021-12-20 23:04:51 -0300

    install intl

--- react-native/gatito/App.js
+++ react-native/gatito/App.js
@@ -1,4 +1,7 @@
 import { SafeAreaView, StatusBar } from "react-native";
+import "intl";
+import "intl/locale-data/jsonp/en.js";
+
 import Services from "./src/screens/Services";
 
 export default function App() {
